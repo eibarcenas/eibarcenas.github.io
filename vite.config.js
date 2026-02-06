60c4544c9112451ccb9f0a6754f6059ef070d7fe
Erick Bárcenas <ebarcenas@rombo.mx>
2026-02-05 21:54:45 -0600

    feat: optimize for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths for deployment
   server: {
     port: 8005, // Keep the same port preference
   },
